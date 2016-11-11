553e316ee02ab8be04a3525398ed39097ca0077b
Reed Jessen <reed@algo.ventures>
2016-11-11 11:07:58 -0800

    added main function to be more pythonic

--- PycharmProjects/ip_data/ip_data.py
+++ PycharmProjects/ip_data/ip_data.py
@@ -15,5 +15,7 @@ def parse_patent_data_file(myfile):
     return dic
 
 
-with open("maps 225719.txt", "r", encoding="utf8")as myfile:
-    parse_patent_data_file(myfile)
+if __name__ == '__main__':
+
+    with open("maps 225719.txt", "r", encoding="utf8")as myfile:
+        parse_patent_data_file(myfile)
