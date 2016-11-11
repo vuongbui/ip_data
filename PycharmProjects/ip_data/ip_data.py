2180b8dbbb74e66839df10143b2065e989307e06
Reed Jessen <reed@algo.ventures>
2016-11-11 11:04:49 -0800

    Moved function definition to top

--- PycharmProjects/ip_data/ip_data.py
+++ PycharmProjects/ip_data/ip_data.py
@@ -1,13 +1,18 @@
+
+
+
+def ip_data(myfile):
+    dic = {}
+    for line in myfile:
+        line = line.strip()
+        if line.startswith("PATA"):
+            dic["publication_number"] = line[7:-2]
+        if line.startswith("TITL"):
+            dic["title"] = line[5:]
+        if line.startswith("ABST"):
+            dic["abstract"] = line[5:]
+    print(dic)
+
+
 with open("maps 225719.txt", "r", encoding="utf8")as myfile:
-    def ip_data(myfile):
-        dic = {}
-        for line in myfile:
-            line = line.strip()
-            if line.startswith("PATA"):
-                dic["publication_number"] = line[7:-2]
-            if line.startswith("TITL"):
-                dic["title"] = line[5:]
-            if line.startswith("ABST"):
-                dic["abstract"] = line[5:]
-        print (dic)
     ip_data(myfile)
